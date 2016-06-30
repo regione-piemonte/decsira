a0c70dac90d21884f3c6f033d738b7e81af9395c
Simone Cornacchia <seancrow76@gmail.com>
2016-06-30 14:07:37 +0200

    - added the possibility to configure a "fallback" RoleService, to use when IRIDE does not retrieve any roles for a given username  - this may change in scope in future, as requirements get clearer - added some configuration validation conditions, to have IrideRoleService fail its initialization as soon as any validation condition fails

---
+++ security/sec-iride/src/main/java/org/geoserver/security/iride/util/builder/Builder.java
@@ -0,0 +1,17 @@
+package org.geoserver.security.iride.util.builder;
+
+/**
+ * 
+ * @author "Simone Cornacchia - seancrow76@gmail.com, simone.cornacchia@consulenti.csi.it (CSI:71740)"
+ *
+ * @param <T>
+ */
+public interface Builder<T> {
+
+    /**
+     *
+     * @return
+     */
+    T build();
+
+}
