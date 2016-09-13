31d995f634dca05a5d810e6ee92c42d73ad63c1f
Tobia Di Pisa <tobia.dipisa@geo-solutions.it>
2016-09-13 11:50:28 +0200

    Fixing the query form reset (#132)

--- js/reducers/queryform.js
+++ js/reducers/queryform.js
@@ -7,6 +7,46 @@
  */
 
 const msQueryFrom = require('../../MapStore2/web/client/reducers/queryform');
+const assign = require('object-assign');
+
+const siraInitialState = {
+    "attributePanelExpanded": true,
+    "filterFields": [
+        {
+            "attribute": null,
+            "exception": null,
+            "groupId": 1,
+            "operator": "=",
+            "rowId": 1,
+            "type": null,
+            "value": null
+        }
+    ],
+    "groupFields": [
+        {
+            "id": 1,
+            "index": 0,
+            "logic": "AND"
+        }
+    ],
+    "groupLevels": 1,
+    "pagination": {
+        "maxFeatures": 15,
+        "startIndex": 0
+    },
+    "searchUrl": "{geoserverUrl}/ows?service=WFS",
+    "showDetailsPanel": false,
+    "showGeneratedFilter": false,
+    "spatialField": {
+        "attribute": "geometria",
+        "geometry": null,
+        "method": null,
+        "operation": "INTERSECTS"
+    },
+    "spatialPanelExpanded": false,
+    "toolbarEnabled": true,
+    "useMapProjection": false
+};
 
 function queryform(state, action) {
     switch (action.type) {
@@ -16,9 +56,11 @@ function queryform(state, action) {
         case 'FEATURETYPE_CONFIG_LOADED': {
             return {...state, spatialField: {...state.spatialField, attribute: action.geometryName}};
         }
+        case 'QUERY_FORM_RESET': {
+            return assign({}, state, siraInitialState);
+        }
         default: return msQueryFrom(state, action);
     }
 }
 
 module.exports = queryform;
-
