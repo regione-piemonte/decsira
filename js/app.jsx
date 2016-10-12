718694b387bdf553f9f0ece7aec06a3392dd1375
unknown <diego.sanmartino@gmail.com>
2016-10-12 11:22:49 +0200

    add call to loadtiles action

--- js/app.jsx
+++ js/app.jsx
@@ -5,67 +5,68 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
  const React = require('react');
-const ReactDOM = require('react-dom');
-const {connect} = require('react-redux');
+ const ReactDOM = require('react-dom');
+ const {connect} = require('react-redux');
 
-const LocaleUtils = require('../MapStore2/web/client/utils/LocaleUtils');
+ const LocaleUtils = require('../MapStore2/web/client/utils/LocaleUtils');
 
-const {loadMapConfig} = require('../MapStore2/web/client/actions/config');
-const {configureQueryForm, configureTopology/*, configureFeatureGrid*/} = require('./actions/siradec');
+ const {loadMapConfig} = require('../MapStore2/web/client/actions/config');
+ const {configureQueryForm, configureTopology/*, configureFeatureGrid*/} = require('./actions/siradec');
+ const {loadTiles} = require('./actions/mosaictile');
 
-const appReducers = {
-    userprofile: require('./reducers/userprofile'),
-    siraControls: require('./reducers/controls'),
-    queryform: require('./reducers/queryform'),
-    siradec: require('./reducers/siradec'),
-    grid: require('./reducers/grid'),
-    cardtemplate: require('./reducers/card'),
-    featuregrid: require('./reducers/featuregrid'),
-    draw: require('../MapStore2/web/client/reducers/draw'),
-    security: require('./reducers/siraSecurity')
+ const appReducers = {
+     userprofile: require('./reducers/userprofile'),
+     siraControls: require('./reducers/controls'),
+     queryform: require('./reducers/queryform'),
+     siradec: require('./reducers/siradec'),
+     grid: require('./reducers/grid'),
+     cardtemplate: require('./reducers/card'),
+     featuregrid: require('./reducers/featuregrid'),
+     draw: require('../MapStore2/web/client/reducers/draw'),
+     security: require('./reducers/siraSecurity'),
+     mosaic: require('./reducers/mosaic')
+ };
 
-};
+ const startApp = () => {
+     const ConfigUtils = require('../MapStore2/web/client/utils/ConfigUtils');
+     const StandardApp = require('../MapStore2/web/client/components/app/StandardApp');
 
-const startApp = () => {
-    const ConfigUtils = require('../MapStore2/web/client/utils/ConfigUtils');
-    const StandardApp = require('../MapStore2/web/client/components/app/StandardApp');
+     const {pages, pluginsDef, initialState, storeOpts} = require('./appConfig');
 
-    const {pages, pluginsDef, initialState, storeOpts} = require('./appConfig');
+     const StandardRouter = connect((state) => ({
+         locale: state.locale || {},
+         pages
+     }))(require('../MapStore2/web/client/components/app/StandardRouter'));
 
-    const StandardRouter = connect((state) => ({
-        locale: state.locale || {},
-        pages
-    }))(require('../MapStore2/web/client/components/app/StandardRouter'));
+     const appStore = require('./stores/store').bind(null, initialState, appReducers);
+     const { configUrl, legacy } = ConfigUtils.getUserConfiguration('config', 'json');
 
-    const appStore = require('./stores/store').bind(null, initialState, appReducers);
-    const { configUrl, legacy } = ConfigUtils.getUserConfiguration('config', 'json');
+     const initialActions = [
+         () => loadTiles(),
+         () => loadMapConfig(configUrl, legacy),
+         () => configureQueryForm(ConfigUtils.getConfigProp("query")),
+         () => configureTopology(ConfigUtils.getConfigProp("topology"))
+     ];
 
-    const initialActions = [
-        () => loadMapConfig(configUrl, legacy),
-        () => configureQueryForm(ConfigUtils.getConfigProp("query")),
-        () => configureTopology(ConfigUtils.getConfigProp("topology"))
-    ];
+     const appConfig = {
+         storeOpts,
+         appStore,
+         pluginsDef,
+         initialActions,
+         appComponent: StandardRouter,
+         printingEnabled: false
+     };
 
-    const appConfig = {
-        storeOpts,
-        appStore,
-        pluginsDef,
-        initialActions,
-        appComponent: StandardRouter,
-        printingEnabled: false
-    };
+     ReactDOM.render(
+         <StandardApp {...appConfig}/>,
+         document.getElementById('container')
+     );
+ };
 
-    ReactDOM.render(
-        <StandardApp {...appConfig}/>,
-        document.getElementById('container')
-    );
-};
-
-if (!global.Intl ) {
-    // Ensure Intl is loaded, then call the given callback
-    LocaleUtils.ensureIntl(startApp);
-}else {
-    startApp();
-}
+ if (!global.Intl ) {
+     // Ensure Intl is loaded, then call the given callback
+     LocaleUtils.ensureIntl(startApp);
+ }else {
+     startApp();
+ }
