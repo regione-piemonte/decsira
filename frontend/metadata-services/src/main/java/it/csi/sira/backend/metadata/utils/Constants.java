0a47866b36c7763a7f6ba4edc4d99d3ad6907391
marcozampa1970 <marco.zampaglione@gmail.com>
2016-11-16 15:32:32 +0100

    Added getPlatformNumbers method in metadata services module

--- frontend/metadata-services/src/main/java/it/csi/sira/backend/metadata/utils/Constants.java
+++ frontend/metadata-services/src/main/java/it/csi/sira/backend/metadata/utils/Constants.java
@@ -7,8 +7,11 @@ public class Constants {
   public static final String METADATA_OBJECTS = "OBJECTS";
   public static final String METADATA_VIEWS = "VIEWS";
   
-  public static final int FUNZIONE_MAPPA_WMS = 1;
-  public static final int FUNZIONE_DOWNLOAD_WFS = 3;  
+  public static final int FUNZIONE_MAPPA = 1;
+  public static final int FUNZIONE_CERCA = 2;  
+  public static final int FUNZIONE_DOWNLOAD = 3;  
+  public static final int FUNZIONE_VISTA = 4;  
+  public static final int FUNZIONE_MAPPA_APPL = 5; 
  
 
 }
