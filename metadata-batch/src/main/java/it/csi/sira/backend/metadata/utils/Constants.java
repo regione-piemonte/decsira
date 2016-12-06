e3d25d0195f3530ef02aa8972be70adefd61e094
marcozampa1970 <marco.zampaglione@gmail.com>
2016-12-06 14:27:34 +0100

    metadata batch: added configuration for batch distribution (issue #300) (#301)
    * added configuration for batch distribution
    
    * added configuration for batch distribution

--- metadata-batch/src/main/java/it/csi/sira/backend/metadata/utils/Constants.java
+++ metadata-batch/src/main/java/it/csi/sira/backend/metadata/utils/Constants.java
@@ -2,7 +2,7 @@ package it.csi.sira.backend.metadata.utils;
 
 public class Constants {
 
-  public final static String LOGGER_NAME = "metadatabatch";
+  public final static String LOGGER_NAME = "decsirabatch";
 
   
   public static final int ID_ROOT_APP_CATEGORY = 1;
