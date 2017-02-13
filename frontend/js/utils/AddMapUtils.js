db31d84af5eb6449ee16c70bd5eec2da00005395
kappu <kappu72gmail.com>
2017-02-13 18:10:03 +0100

    Addede online resource

--- frontend/js/utils/AddMapUtils.js
+++ frontend/js/utils/AddMapUtils.js
@@ -51,7 +51,7 @@ const AddMapUtils = {
                 props.group = group;
                 props.groupTitle = groupTitle;
                 props.nodetype = 'layer';
-                props.url = wmsUrl;
+                props.url = r.onlineResource && r.onlineResource["xlink:href"] || wmsUrl;
                 return acc.concat(assign({}, r, props));
             }
             props.nodetype = 'group';
@@ -100,7 +100,7 @@ const AddMapUtils = {
             if (srs > 0 && !CoordinatesUtils.isAllowedSRS(projection, allowedSRS)) {
                 reject(new Error("'catalog.srs_not_allowed"));
             }
-            const nodeGroup = node.title;
+            const nodeGroup = (node.title || "").replace(/\./g, '${dot}');
             const group = useTitle ? layer.groupTitle : layer.group;
             resolve(assign({}, {
                     url: layer.url,
