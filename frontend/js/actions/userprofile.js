cf59a5b8574c1249bee1f8a86477af7a41ba42a4
Francesca Morelli <francesca.morelli@csi.it>
2017-09-01 10:57:11 +0200

    corretto le segnalazioni dopo il primo rilascio in prod

--- frontend/js/actions/userprofile.js
+++ frontend/js/actions/userprofile.js
@@ -63,7 +63,7 @@ function loadUserIdentity(serviceUrl = 'services/iride/getRolesForDigitalIdentit
             if (typeof response.data === 'object') {
                 if (response.data.userIdentity && response.data.roles && response.data.roles.length > 0) {
                     // there is a logged user, geoserverUrl = secureGeoserverUrl
-                    ConfigUtils.setConfigProp('geoserverUrl', ConfigUtils.getConfigProp('secureGeoserverUrl'));
+                     ConfigUtils.setConfigProp('geoserverUrl', ConfigUtils.getConfigProp('secureGeoserverUrl'));
                     response.data.profile = [];
                     Array.from(response.data.roles).forEach(function(val) {
                         if (val && val.mnemonic) {
