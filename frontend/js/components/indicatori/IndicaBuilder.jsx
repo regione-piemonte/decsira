ab5642bd826478c6b051c686ebd36b8044c99198
Valeria Foco <valeria.foco@consoft.it>
2021-02-25 09:29:03 +0100

    viewparams salvati sul layer in toc

--- frontend/js/components/indicatori/IndicaBuilder.jsx
+++ frontend/js/components/indicatori/IndicaBuilder.jsx
@@ -300,27 +300,6 @@ function IndicaBuilder({
         });
     }
 
-    /*let indicaFilters = [
-        {
-            attribute:"decsiraogc_indicatori_ru:fk_ris_spaziale",
-            operator:"=",
-            type:"number",
-            value: selectedRisSpaziale.id
-        },
-        {
-            attribute:"decsiraogc_indicatori_ru:fk_dimensione",
-            operator:"=",
-            type:"number",
-            value: selectedIndicatore.id
-        },
-        {
-            attribute:"decsiraogc_indicatori_ru:fk_ambito_temporale",
-            operator:"=",
-            type:"number",
-            value: selectedDettaglioPeriodicita.id
-        }
-    ];*/
-
     function applyStyle() {
         if(!(selectedRisSpaziale.id && selectedIndicatore.id && selectedDettaglioPeriodicita.id)) {
             return;
@@ -340,8 +319,7 @@ function IndicaBuilder({
             let bodyData = resp.data.replace('<?xml version="1.0" encoding="UTF-8"?>', '');
             wmsLayer.title = getWmsTitle();
             wmsLayer.params = assign({}, wmsLayer.params, {SLD_BODY: bodyData});
-            //wmsLayer.viewparams = getViewParams();
-            //wmsLayer.indicaFilters = indicaFilters;
+            wmsLayer.viewparams = getViewParams();
             addIndicaLayer(wmsLayer);
         }).catch(e => {
             console.error(e);
