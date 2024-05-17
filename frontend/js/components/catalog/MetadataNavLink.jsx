68a0f9954412ab726958911cd1ec7a536f76b9d0
FALCONE Emmanuel 2145 <emmanuel.falcone@csi.it>
2024-05-17 14:11:46 +0200

    aggiunto barra dei link per i metadata

---
+++ frontend/js/components/catalog/MetadataNavLink.jsx
@@ -0,0 +1,40 @@
+const React = require('react');
+const I18N = require('@mapstore/components/I18N/I18N');
+
+class MetadataNavLink extends React.Component {
+    render() {
+        let { provider, urlMetadato, wfsUrl, wmsUrl } = this.props;
+
+
+        return (
+            <div className="containerDefaultNodeFooter handleMetadato ">
+                <p>
+                    <strong><I18N.Message msgId={"metadataInfoBox.entePA"} /></strong>
+                    {provider}
+                </p>
+                <p>
+                    <strong><I18N.Message msgId={"metadataInfoBox.urlMetadato"} /></strong>
+                    <a target="_blank" rel="noopener noreferrer" href={urlMetadato}>
+                        <I18N.Message msgId={"metadataInfoBox.goToMetadato"} />
+                    </a>
+                </p>
+                <p>
+                    {wfsUrl}
+                    {wmsUrl}
+                </p>
+                <p>
+                    <button
+                        className="btn btn-link">
+                        <I18N.Message msgId={"metadataInfoBox.showLegendButton"} />
+                    </button>
+                </p>
+
+
+            </div>
+        );
+    }
+}
+
+
+
+module.exports = MetadataNavLink;
