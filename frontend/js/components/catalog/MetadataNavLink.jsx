1161161ed887dd8250b07bad50fad6a389eea9c4
FOCO Valeria 2221 <valeria.foco@csi.it>
2024-05-17 16:17:36 +0200

    fix bottone login

--- frontend/js/components/catalog/MetadataNavLink.jsx
+++ frontend/js/components/catalog/MetadataNavLink.jsx
@@ -1,7 +1,15 @@
 const React = require('react');
 const I18N = require('@mapstore/components/I18N/I18N');
+const PropTypes = require('prop-types');
 
 class MetadataNavLink extends React.Component {
+    static propTypes = {
+        buttonLegend: PropTypes.object,
+        wfsUrl: PropTypes.object,
+        wmsUrl: PropTypes.object,
+        urlMetadato: PropTypes.object,
+        provider: PropTypes.object
+    };
 
     render() {
         let { provider, urlMetadato, wfsUrl, wmsUrl, buttonLegend } = this.props;
